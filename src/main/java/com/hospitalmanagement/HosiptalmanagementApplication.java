8e5a148d0401fd2ef43cb9d2684cc1b728562332
ariftxt <arif.ali@xyloop.com>
2025-08-25 17:10:32 +0530

    Initial Spring Boot project structure

---
+++ src/main/java/com/hospitalmanagement/HosiptalmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.hospitalmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HosiptalmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HosiptalmanagementApplication.class, args);
+	}
+
+}
