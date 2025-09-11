a326389df04c0a667703cbb7321587b2f800cd9a
mdarifali786 <arif.ali@xyloop.com>
2025-09-11 23:15:45 +0530

    refactor roleType enum

--- src/main/java/com/hospitalManagement/entity/Role.java
+++ src/main/java/com/hospitalManagement/entity/Role.java
@@ -1,5 +1,6 @@
 package com.hospitalManagement.entity;
 
+import com.hospitalManagement.enums.RoleType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
